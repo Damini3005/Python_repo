c54075f7ce01fc08f645a391f899dae740d69c2a
developer <daminipatil2910@gmail.com>
2025-09-25 17:18:00 +0530

    done:check type of numbers

---
+++ CheatsheetPractice_example/day1_practice.py
@@ -0,0 +1,9 @@
+# -----------------#
+print(type(1)) 
+# print(type(1+ '10'))  #it shows error:- TypeError: unsupported operand type(s) for +: 'int' and 'str'
+print(type(-20)) 
+print(type(1 - 20)) 
+print(type(100.0)) 
+print(type(2.10)) 
+print(type(4E2)) 
+print(type(1E10)) 
