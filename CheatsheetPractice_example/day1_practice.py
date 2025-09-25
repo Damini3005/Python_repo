8d7e9dc0ab89fb3dfba6809357b05b588691cf04
developer <daminipatil2910@gmail.com>
2025-09-25 17:30:52 +0530

    done:check arithmatic operators

--- CheatsheetPractice_example/day1_practice.py
+++ CheatsheetPractice_example/day1_practice.py
@@ -1,4 +1,4 @@
-# -----------------#
+# --------Type of numbers---------#
 print(type(1)) 
 # print(type(1+ '10'))  #it shows error:- TypeError: unsupported operand type(s) for +: 'int' and 'str'
 print(type(-20)) 
@@ -7,3 +7,12 @@ print(type(100.0))
 print(type(2.10)) 
 print(type(4E2)) 
 print(type(1E10)) 
+
+#---------Arithmatic----------#
+print(100+10)
+print(10-5)
+print(100*2)
+print(10**3)
+print(10/3)
+print(100 // 2)
+print(10 % 3)
